96ed40947665343e81e6c62c7e8687f9691be833
elvstejd <elvistejeda0910@gmail.com>
2021-10-23 12:50:55 -0400

    Remove import

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var userModel = require('../models/user.js');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
